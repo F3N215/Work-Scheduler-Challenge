d8c9914f228aa940b7f8686da3a6736466c2f73d
Daniel John <dcampo85@gmail.com>
2023-12-14 07:45:49 -0500

    reviewing js

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -7,21 +7,19 @@ function displayTime() {
   timeDisplayEl.text(rightNow);
 }
 
-
-$(document).ready(function () {
-var showTime = document.querySelector("#currentDay"); // returns current date/time
+/* $(document).ready (function() {
+var showTime = document.querySelector("#currentDay"); // returns current date/time */
 
 $document.ready (function() {
+
 var showTime = document.querySelector("#currentDay"); // displays current date/time
-
 var currentTime = dayjs().format("dddd,MMMM D, YYYY, h:mm:ss a");
-
 showTime.textContent = currentTime;
 
-$(".saveBtn").on("click", function(){
+$(".saveBtn").on("click", function(){ // add event listener, returns row id
   var text = $(this).sublings(".description").val();
   var time = $(this).parent().attr("id");
-  localStorage.setItem(time, text);
+  localStorage.setItem(time, text); // saves row id to local storage
 });
 })
 
@@ -50,7 +48,9 @@ trackHours();
 $(function () {});
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
+  // local storage. <-- DONE
+  
+  // HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
@@ -58,7 +58,9 @@ $(function () {});
 
 
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
+  // block by comparing the id to the current hour. 
+  
+  // HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
