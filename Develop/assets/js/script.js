9cdecb906e1b694c635604fabae24a60b72c4d9d
Daniel John <dcampo85@gmail.com>
2023-12-15 17:04:23 -0500

    tutor session 3

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,5 +1,4 @@
 // Use const until you can't, and then use let 
-
 $(document).ready(function() {
 
 function displayDate() {
@@ -72,10 +71,3 @@ function displayTime() {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
